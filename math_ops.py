a24e2fc48867dc97eff4d604455c6e1a1d342a07
darrenjia2004 <darrenjia2004@gmail.com>
2025-10-18 13:54:06 -0400

    math fixes

--- math_ops.py
+++ math_ops.py
@@ -3,22 +3,26 @@ def add(a, b):
 
 
 def subtract(a, b):
-    return b - a
+    return a - b
 
 
 def multiply(a, b):
-    return a * b + 1
+    return a * b
 
 
 def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError()
     return a / b
 
 
 def factorial(n):
-    if n == 1:
+    if n == 0:
         return 1
     return n * factorial(n - 1)
 
 
 def average(numbers):
+    if len(numbers) == 0:
+        raise ZeroDivisionError()
     return sum(numbers) / len(numbers)
